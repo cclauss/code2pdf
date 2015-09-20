e9bc689799515ed9e2623872dca56001e0334cd8
Tushar Gautam <tushar.rishav@gmail.com>
2015-09-20 18:31:19 +0530

    bug to be fixed later

---
+++ build/lib.linux-x86_64-2.7/Py2pdf/__init__.py
@@ -0,0 +1,1 @@
+from py2pdf import main
--- setup.py
+++ setup.py
@@ -3,19 +3,22 @@ try:
 except ImportError:
     from distutils.core import setup
 
-import sys
+import sys,os
+# cur_dir = os.path.abspath(os.path.dirname(__file__))
+# css_file = os.path.join(cur_dir, 'Py2pdf/css/','py2html.css')
+# print css_file
 
 extra = {}
 if sys.version_info >= (3,):
     extra['use_2to3'] = True
 setup(name='Py2pdf',
-      version='0.0.2',
+      version='0.0.16',
       install_requires=[
           r for r in open('requirements.txt', 'r').read().split('\n') if r],
       author='Tushar Gautam',
       author_email='tushar.rishav@gmail.com',
       packages=['Py2pdf', ],
-      data_files=[('', ['py2html.css'])],
+      data_files=[('', ['Py2pdf/css/py2html.css'])],
       entry_points={
           'console_scripts': ['py2pdf=Py2pdf:main'],
       },
